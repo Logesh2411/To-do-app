fccf4ca3a093199e1474e9b928e98b7cb7b158aa
Logesh-graas <logeshwaran.k@graas.ai>
2024-05-09 15:36:25 +0530

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TodoWrapper from './Components/TodoWrapper';
+
+
+
+const App =() => {
+  return (
+    <TodoWrapper />
+  );
+}
+
+export default App;
---
+++ my-app/src/Components/TodoForm.jsx
@@ -0,0 +1,30 @@
+import React,{useState} from "react";
+import './Styles/TodoForm.scss'
+
+const TodoForm = ({addTodo}) =>
+    { 
+        const [value,setValue] = useState("");
+
+        const handleSubmit = e => {
+            e.preventDefault();
+            addTodo(value);
+            setValue("")
+        }
+
+        return(
+            <>
+                <form className="todoForm"
+                    onSubmit={handleSubmit}>
+                    <input type="text" 
+                    placeholder="What are the task today?" 
+                    className="todo-input"
+                    value={value}
+                    onChange={(e)=>setValue(e.target.value)}
+                    />
+                    <button type="submit" className="todo-btn">Add</button>
+                </form>
+            </>
+        )
+    }
+
+    export  default TodoForm;
